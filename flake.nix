d8c2cc5cc513e24734dd29b73291b8b3e2675dfc
bhaswata <bhaswata08@gmail.com>
2026-09-08 13:04:35 +0530

    release: 0.2.0

--- flake.nix
+++ flake.nix
@@ -37,7 +37,7 @@
       packages = forAll (pkgs: {
         default = pkgs.python313Packages.buildPythonApplication {
           pname = "torchtyc";
-          version = "0.1.2";
+          version = "0.2.0";
           pyproject = true;
           src = self;
           build-system = [ pkgs.python313Packages.hatchling ];
--- src/torchtyc/__init__.py
+++ src/torchtyc/__init__.py
@@ -4,7 +4,7 @@ from .config import Config, load
 from .diagnostics import RULES, Diagnostic, Rule, Severity
 from .engine import Report, check_paths, collect_files
 
-__version__ = "0.1.2"
+__version__ = "0.2.0"
 
 __all__ = [
     "RULES",
